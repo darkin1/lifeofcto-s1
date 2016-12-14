bb8aba1b98225cf36f19593975688cfbdec848a7
dariusz <dciesielski87@gmail.com>
2016-12-14 22:29:43 +0100

    s01e04

--- project/index.php
+++ project/index.php
@@ -1,2 +1,13 @@
 <?php
 echo 'test';
+
+$db = mysqli_connect('mysql', 'root', 'toor', 'lifeofcto') or die('Error');
+$query = mysqli_query($db, 'show databases');
+$results = mysqli_fetch_array($query);
+
+echo '<pre>'; var_dump($results); echo '</pre>';
+
+$redis = new Redis();
+$redis->connect('redis', 6379);
+echo "Connection to server sucessfully";
+echo "Server is running: ".$redis->ping();
